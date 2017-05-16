1faab2effdf14267f9fd5faaa43c5a9b185b1408
eduardodmlm <eduardodmlm@gmail.com>
2017-05-16 23:46:56 +0100

    adicionados alguns conteudos ao frontend, necessario melhorar

--- JoiaDaEuropa/db/schema.rb
+++ JoiaDaEuropa/db/schema.rb
@@ -12,6 +12,20 @@
 
 ActiveRecord::Schema.define(version: 20170516091846) do
 
+  create_table "client_area_order_states", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "client_area_orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "date"
+    t.decimal  "total",      precision: 10
+    t.integer  "state_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
   create_table "order_states", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "state"
     t.datetime "created_at", null: false
